883928bb94c1312764560ede9a50b8040e473bfb
Morgan Stockham <morganstockham@gmail.com>
2020-06-29 13:28:56 -0400

    maps now reflect 10-15-20 intervals

--- src/isochrones.R
+++ src/isochrones.R
@@ -34,47 +34,48 @@ shp_to_sf <- function(folder, file){
 }
 
 emsstations <- shp_to_sf("emsstations","emsstations")
-#emsstations leaflet plot ---------------------------------------------------------
-emsstations_plot <- leaflet(data = emsstations) %>% # create leaflet object
-  addProviderTiles(provider = "CartoDB.Positron") %>% # add basemap
-  addMarkers()
-#call plot
-emsstations_plot
+
+# emsstations_plot <- leaflet(data = emsstations) %>% # create leaflet object
+#   addProviderTiles(provider = "CartoDB.Positron") %>% # add basemap
+#   addMarkers()
+# #call plot
+# emsstations_plot
 
 # traveltime ----------------------------------------------------------------------
 readRenviron("~/.Renviron")
 traveltime_api <- Sys.getenv("TRAVELAPI")
 traveltime_id <- Sys.getenv("TRAVELID")
 
+
 for(i in 1:nrow(emsstations)){
-  traveltime5 <- traveltime_map(appId= traveltime_id,
+  traveltime10 <- traveltime_map(appId= traveltime_id,
                                 apiKey = traveltime_api,
-                                location=c(emsstations$LATITUDE[1],emsstations$LONGITUDE[1]),
-                                traveltime=300,
+                                location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
+                                traveltime=600,
                                 type="driving",
                                 departure="2020-08-07T08:00:00+01:00")
-  traveltime10 <- traveltime_map(appId= traveltime_id,
-                                 apiKey = traveltime_api,
-                                 location=c(emsstations$LATITUDE[1],emsstations$LONGITUDE[1]),
-                                 traveltime=600,
-                                 type="driving",
-                                 departure="2020-08-07T08:00:00+01:00")
   traveltime15 <- traveltime_map(appId= traveltime_id,
                                  apiKey = traveltime_api,
-                                 location=c(emsstations$LATITUDE[1],emsstations$LONGITUDE[1]),
+                                 location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
                                  traveltime=900,
                                  type="driving",
                                  departure="2020-08-07T08:00:00+01:00")
-  m1 = mapview(traveltime5,  col.regions = c("grey"))
+  traveltime20 <- traveltime_map(appId= traveltime_id,
+                                 apiKey = traveltime_api,
+                                 location=c(emsstations$LATITUDE[i],emsstations$LONGITUDE[i]),
+                                 traveltime=1200,
+                                 type="driving",
+                                 departure="2020-08-07T08:00:00+01:00")
+  m1 = mapview(traveltime10,  col.regions = c("grey"))
   # add the second layer on top
-  m1 = m1 + traveltime10 + traveltime15
+  m1 = m1 + traveltime15 + traveltime20
   mapshot(m1, file = paste0("~/git/dspg2020patrick/output/isochrone_maps/emsmap_",i, ".png", sep = ""))
 }
 
 # osrm ------------------------------------------------------------------------
 
 for(i in 1:nrow(emsstations)){
-iso <- osrmIsochrone(loc = c(emsstations$LONGITUDE[i], emsstations$LATITUDE[i]), breaks = seq(from = 0,to = 15, by = 5))
+iso <- osrmIsochrone(loc = c(emsstations$LONGITUDE[i], emsstations$LATITUDE[i]), breaks = seq(from = 10,to = 20, by = 5))
 
 iso@data$drive_times <- factor(paste(iso@data$min, "to", iso@data$max, "min"))
 # color palette for each area
@@ -84,7 +85,7 @@ factpal <- colorFactor(rev(heat.colors(3)), iso@data$drive_times)
 m2 <- leaflet() %>% 
   setView(emsstations$LONGITUDE[i], emsstations$LATITUDE[i], zoom = 11) %>%
   addProviderTiles("CartoDB.Positron", group="Greyscale") %>% 
-  addMarkers(lng = emsstations$LONGITUDE[i], emsstations$LATITUDE[i], popup = "1st EMS Station") %>% 
+  addMarkers(lng = emsstations$LONGITUDE[i], emsstations$LATITUDE[i], popup = "EMS Station Isochrone") %>% 
   addPolygons(fill=TRUE, stroke=TRUE, color = "black",
               fillColor = ~factpal(iso@data$drive_times),
               weight=0.5, fillOpacity=0.2,
