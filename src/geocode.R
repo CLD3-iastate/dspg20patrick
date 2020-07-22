4530c37528772191fb424b81a13d65f992bd5c20
Teja Pristavec <tp2sk@virginia.edu>
2020-07-22 11:54:39 -0400

    Updated wifi and grocery geocodes

--- src/geocode.R
+++ src/geocode.R
@@ -5,6 +5,7 @@ library(sf)
 library(leaflet)
 library(disco)
 library(RColorBrewer)
+library(readr)
 
 
 #
@@ -27,15 +28,20 @@ wifi$latitude[5] <- 36.624179
 wifi$longitude[5] <- -80.269961
 wifi$geo_method[5] <- "manual"
 
+# Patrick Henry Community College Stuart Campus, lat = 36.647241, long = -80.270562
+wifi$latitude[10] <- 36.647241
+wifi$longitude[10] <- -80.270562
+wifi$geo_method[10] <- "manual"
+
 # Meadows of Dan Food Market, lat = 36.735641, long = -80.407796
-groceries$latitude[12] <- 36.735641
-groceries$longitude[12] <- -80.407796
-groceries$geo_method[12] <- "manual"
+groceries$latitude[6] <- 36.735641
+groceries$longitude[6] <- -80.407796
+groceries$geo_method[6] <- "manual"
 
 # Country Convenience Market, lat = 36.741524, long = -80.208900
-groceries$latitude[16] <- 36.741524
-groceries$longitude[16] <- -80.208900
-groceries$geo_method[16] <- "manual"
+groceries$latitude[10] <- 36.741524
+groceries$longitude[10] <- -80.208900
+groceries$geo_method[10] <- "manual"
 
 
 #
@@ -55,7 +61,7 @@ leaflet(groceries) %>%
 
 # Wifi
 leaflet(wifi) %>%
-  addTiles() %>%
+  addProviderTiles(providers$CartoDB.Positron) %>%
   addCircleMarkers(stroke = FALSE, fillOpacity = 1, radius = 6)
 
 
@@ -67,7 +73,6 @@ write.csv(groceries, "./data/working/geocode/patrick_groceries.csv")
 write.csv(wifi, "./data/working/geocode/patrick_wifi.csv")
 
 groceries <- groceries %>% select(-notes)
-wifi <- wifi %>% select(-notes)
 
 write_rds(groceries, "./data/web/groceries.Rds")
 write_rds(wifi, "./data/web/wifi.Rds")
